51b697549486f065ef38a50585c955f669c2cff4
mfaishaldp <faishaldarmaputra@gmail.com>
2025-04-30 09:11:22 +0700

    fix : validation in plan models

--- server/models/plan.js
+++ server/models/plan.js
@@ -18,14 +18,102 @@ module.exports = (sequelize, DataTypes) => {
   Plan.init({
     UserId: DataTypes.INTEGER,
     StatusId : DataTypes.INTEGER,
-    longitudeLocation: DataTypes.FLOAT,
-    latitudeLocation: DataTypes.FLOAT,
-    longitudeDestination: DataTypes.FLOAT,
-    latitudeDestination: DataTypes.FLOAT,
-    displayNameLocation: DataTypes.STRING,
-    displayNameDestination: DataTypes.STRING,
-    recommendationItems: DataTypes.STRING,
-    timeTemperaturePredicted: DataTypes.STRING
+    longitudeLocation: {
+      type : DataTypes.FLOAT,
+      allowNull : false,
+      validate : {
+        notEmpty : {
+          msg : 'Longitude Location is required'
+        },
+        notNull : {
+          msg : 'Longitude Location is required'
+        }
+      }
+    },
+    latitudeLocation: {
+      type : DataTypes.FLOAT,
+      allowNull : false,
+      validate : {
+        notEmpty : {
+          msg : 'Latitude Location is required'
+        },
+        notNull : {
+          msg : 'Latitude Location is required'
+        }
+      }
+    },
+    longitudeDestination: {
+      type : DataTypes.FLOAT,
+      allowNull : false,
+      validate : {
+        notEmpty : {
+          msg : 'Longitude Destination is required'
+        },
+        notNull : {
+          msg : 'Longitude Destination is required'
+        }
+      }
+    },
+    latitudeDestination: {
+      type : DataTypes.FLOAT,
+      allowNull : false,
+      validate : {
+        notEmpty : {
+          msg : 'Latitude Destination is required'
+        },
+        notNull : {
+          msg : 'Latitude Destination is required'
+        }
+      }
+    },
+    displayNameLocation: {
+      type : DataTypes.STRING,
+      allowNull : false,
+      validate : {
+        notEmpty : {
+          msg : 'Location Name is required'
+        },
+        notNull : {
+          msg : 'Location Name is required'
+        }
+      }
+    },
+    displayNameDestination: {
+      type : DataTypes.STRING,
+      allowNull : false,
+      validate : {
+        notEmpty : {
+          msg : 'Destination Name is required'
+        },
+        notNull : {
+          msg : 'Destination Name is required'
+        }
+      }
+    },
+    recommendationItems: {
+      type : DataTypes.STRING,
+      allowNull : false,
+      validate : {
+        notEmpty : {
+          msg : 'Recommendation Item is required'
+        },
+        notNull : {
+          msg : 'Recommendation Item is required'
+        }
+      }
+    },
+    timeTemperaturePredicted: {
+      type : DataTypes.STRING,
+      allowNull : false,
+      validate : {
+        notEmpty : {
+          msg : 'Time and Temperature is required'
+        },
+        notNull : {
+          msg : 'Time and Temperature is required'
+        }
+      }
+    }
   }, {
     sequelize,
     modelName: 'Plan'
