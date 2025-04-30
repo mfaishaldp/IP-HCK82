d242bb89f38afc24acb5133cb58af043c32888cf
mfaishaldp <faishaldarmaputra@gmail.com>
2025-04-30 15:58:50 +0700

    feat : add new navbar component

--- client/outfit-recommendation-project/src/App.jsx
+++ client/outfit-recommendation-project/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import Login from './pages/Login'
 import Register from './pages/register'
 import Home from './pages/Home'
+import MainLayout from './layouts/MainLayout'
+import AuthLayout from './layouts/AuthLayout'
 
 function App() {
 
@@ -12,9 +14,16 @@ function App() {
       <BrowserRouter>
         
         <Routes>
-          <Route path='/login' element={<Login/>} />
-          <Route path='/register' element={<Register/>} />
-          <Route path='/' element={<Home/>} />
+
+          <Route element={<AuthLayout/>}>
+            <Route path='/login' element={<Login/>} />
+            <Route path='/register' element={<Register/>} />
+          </Route>
+
+          <Route element={<MainLayout/>}>
+            <Route path='/' element={<Home/>} />
+          </Route>
+          
         </Routes>
 
       </BrowserRouter>
---
+++ client/outfit-recommendation-project/src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import { useNavigate } from "react-router"
+
+export default function Navbar () {
+    const navigate = useNavigate()
+    return (
+        <>
+        
+        <div className="navbar bg-base-100 shadow-sm bg-color-brown">
+            <div className="navbar-start">
+                <div className="dropdown">
+                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+                </div>
+                <ul
+                    tabIndex={0}
+                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                    <li><a>Home</a></li>
+                    <li><a>My Plan</a></li>
+                </ul>
+                </div>
+            </div>
+            <div className="navbar-center">
+                <a className="btn btn-ghost text-xl">daisyUI</a>
+            </div>
+            <div className="navbar-end">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Home</a></li>
+                    <li>
+                        <details>
+                        <summary>Profile</summary>
+                        <ul className="bg-base-100 rounded-t-none p-2">
+                            <li><a onClick={() => {navigate('/login');localStorage.removeItem("access_token")}}>Logout</a></li>
+                            {/* <li><a>Link 2</a></li> */}
+                        </ul>
+                        </details>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        
+        </>
+    )
+}
