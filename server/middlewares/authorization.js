7b817b3acdfcef7cee971b876096542c7acddf80
mfaishaldp <faishaldarmaputra@gmail.com>
2025-04-30 10:06:02 +0700

    feat : add new feature for authorization guardUser

--- server/middlewares/authorization.js
+++ server/middlewares/authorization.js
@@ -1,7 +1,23 @@
-async function authorization(req,res,next) {
+const {Plan} = require('../models')
+
+async function guardUser(req,res,next) {
     try {
-        
+        const {id : userId} = req.user
+        const {id : planId} = req.params
+
+        const data = await Plan.findByPk(+planId)
+
+        if (data.UserId !== userId) {
+            throw {name : 'Forbidden', message : 'Forbidden Access'}
+        }
+
+        next()
+
     } catch (error) {
         next(error)
     }
+}
+
+module.exports = {
+    guardUser
 }
--- server/routers/plan.js
+++ server/routers/plan.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const { PlanController } = require('../controllers/PlanController')
+const { guardUser } = require('../middlewares/authorization')
 const plan = express.Router()
 
 plan.post('/add-plan', PlanController.createPlan) //! to add new plan
 
 plan.get('/user', PlanController.getPlanByUserId) //! to get plan by user id, need authorization
-plan.delete('/delete/:id', PlanController.delPlanById) //! to delete plan by id, need authorization
-plan.put('/update/:id', PlanController.updateStatusByPlanId) //! to update status plan by id, need authorization
+plan.delete('/:id', guardUser, PlanController.delPlanById) //! to delete plan by id, need authorization
+plan.put('/:id', guardUser, PlanController.updateStatusByPlanId) //! to update status plan by id, need authorization
 
 plan.get('/:id', PlanController.getPlanById) //! to get plan by id
 
